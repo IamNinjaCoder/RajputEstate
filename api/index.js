429e71dde854bade30369b41365028fe98f05063
IamNinjaCoder <bcodingninja@gmail.com>
2024-01-28 19:24:35 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDb!");
+}).catch((err)=>{
+    console.log("Not Connected!!")
+});
 
 const app = express();
 
 app.listen(3004,()=>{
-    console.log("Server is sdfsrunning");
+    console.log("Servefdr is sdfsrunning");
 });
