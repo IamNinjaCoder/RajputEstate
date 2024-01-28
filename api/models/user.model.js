4b17149f55b8c44a72ba9bb2a8f6142904de23ae
IamNinjaCoder <bcodingninja@gmail.com>
2024-01-28 19:42:11 +0530

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from mongoose;
+
+// In this section we are creating some rules that user need to follow while creating data Like creating username...
+//rules are called as Schema 
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String, // only String is required.
+        required:true, //It is must to provide 
+        unique:true, // Only unique Username is allowed
+    },
+
+    email:{
+        type:String, 
+        required:true, 
+        unique:true, 
+    },
+    password:{
+        type:String, 
+        required:true, 
+    
+    }
+},{timestamps:true},);
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
