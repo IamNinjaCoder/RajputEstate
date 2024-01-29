e0b590f0258c41ec078df3f0d9876fccfdf13db9
IamNinjaCoder <bcodingninja@gmail.com>
2024-01-29 10:50:24 +0530

    create middleware function to handle errors

--- api/index.js
+++ api/index.js
@@ -41,3 +41,14 @@ app.listen(3004,()=>{
 app.use("/api/user",userRouter);
 
 app.use('/api/auth',authRouter);
+
+//Creating a middleware and a functioin to handle possible errors
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Sever Error :(";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
