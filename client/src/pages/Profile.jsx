e4b7a534e369172372b79a00e6680ffee1d4b515
IamNinjaCoder <bcodingninja@gmail.com>
2024-02-02 19:27:20 +0530

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,35 +1,40 @@
-import { useSelector } from 'react-redux';
-import { useRef, useEffect,useState } from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import { useRef, useEffect, useState } from 'react';
 import {
   getDownloadURL,
   getStorage,
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
+
+import { updateUserStart,updateUserSuccess,updateUserFailure } from '../redux/user/userSlice';
 import { app } from '../firebase';
+
+
 
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser,loading,error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
-  const [formData,setFormData] = useState({});
-  const [fileUploadError,setFileUploadError]  = useState(false);
+  const [formData, setFormData] = useState({});
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
-  
-
-  useEffect(()=>{
-    if (file){
+  useEffect(() => {
+    if (file) {
       handleFileUpload(file);
     }
-  },[file]);
+  }, [file]);
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
     const fileName = new Date().getTime() + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
+     
 
     uploadTask.on(
       'state_changed',
@@ -49,11 +54,40 @@ export default function Profile() {
     );
   };
 
+  const handleChange = (e) => {
+    
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type='file'
@@ -68,7 +102,7 @@ export default function Profile() {
           className='rounded-full h-24 w-24 object-cover cursor-pointer self-center'
         />
 
-<p className='text-sm self-center'>
+        <p className='text-sm self-center'>
           {fileUploadError ? (
             <span className='text-red-700'>
               Error Image upload (image must be less than 2 mb)
@@ -85,23 +119,28 @@ export default function Profile() {
         <input
           type='text'
           placeholder='username'
+          defaultValue={currentUser.username}
           className='border p-3 rounded-lg'
           id='username'
+          onChange={handleChange}
         />
         <input
           type='text'
           placeholder='email'
+          defaultValue={currentUser.email}
           className='border p-3 rounded-lg'
           id='email'
+          onChange={handleChange}
         />
         <input
           type='text'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
+          onChange={handleChange}
         />
-        <button className='bg-blue-500 text-white rounded-lg p-3 hover:opacity-90 uppercase'>
-          Update
+        <button disabled={loading} className='bg-blue-500 text-white rounded-lg p-3 hover:opacity-90 uppercase'>
+          {loading?'Loading...':'Update'}
         </button>
       </form>
 
@@ -109,6 +148,10 @@ export default function Profile() {
         <span className='text-red-500 cursor-pointer'>Delete Account</span>
         <span className='text-red-500 cursor-pointer'>Sign Out</span>
       </div>
+      <p className='text-red-700 mt-3'>{error?error:''}</p>
+      <p hidden={!updateSuccess} className='text-green-500 mt-3'>{updateSuccess?'Updated Successfully':''}</p>
+      
+
     </div>
   );
 }
