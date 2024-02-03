5f426a0eadd779c3c680e0e020f05eee11dba408
IamNinjaCoder <bcodingninja@gmail.com>
2024-02-03 09:31:44 +0530

    Create Listing Api Route

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 
@@ -44,6 +45,7 @@ app.listen(3004,()=>{
 app.use("/api/user",userRouter);
 
 app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
 
 
 //Creating a middleware and a functioin to handle possible errors
