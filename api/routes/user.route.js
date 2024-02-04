4cf0f09070f0fb677621158c0f53c702e8dc4d60
IamNinjaCoder <bcodingninja@gmail.com>
2024-02-04 09:24:12 +0530

    create get user listing api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -10,7 +10,7 @@
 
 
 import express from 'express';
-import {test} from '../controllers/user.controller.js'
+import {test,getUserListings} from '../controllers/user.controller.js'
 import { updateUser,deleteUser } from '../controllers/user.controller.js';
 import {verifyToken} from '../utils/verifyUser.js';
 
@@ -22,6 +22,7 @@ const router = express.Router();
 router.get('/test',test);
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser);
+router.get('/listings/:id',verifyToken,getUserListings);
 
 
 // This line of code exports the router object to be used in other files. 
